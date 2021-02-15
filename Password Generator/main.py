a47b3408d8a0d7c7ed6dd847192a70f5c5cff87d
Lionel Delamare <lionel.delamare>
2021-02-15 21:53:07 +0100

    Hangman Project Done

--- Password Generator/main.py
+++ Password Generator/main.py
@@ -9,14 +9,31 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 
-password = ""
+# password = ""
+# for string in range(1, nr_letters + 1):
+#     password += random.choice(letters)
+
+# for number in range(1, nr_numbers + 1):
+#     password += random.choice(numbers)
+
+# for symbol in range(1, nr_symbols + 1):
+#     password += random.choice(symbols)
+
+# print(password)
+
+# Hard level
+# Générer un MDP complètement aléatoire
+
+password_random = []
 for string in range(1, nr_letters + 1):
-    password += random.choice(letters)
+    password_random.append(random.choice(letters))
 
 for number in range(1, nr_numbers + 1):
-    password += random.choice(numbers)
+    password_random.append(random.choice(numbers))
 
 for symbol in range(1, nr_symbols + 1):
-    password += random.choice(symbols)
+    password_random.append(random.choice(symbols))
 
-print(password)
+random.shuffle(password_random)
+for password in password_random:
+    print(password, end="")
